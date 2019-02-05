b9ee9d89ffebc487edbff0d4c3ed08c739c8f8c5
EbrahemKreem <ebrahemkreem40@gmail.com>
2019-02-05 21:56:05 +0300

    helloo

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,12 +3,15 @@ import React from 'react';
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
+      <label htmlFor='search'>Search for a robot
       <input
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
         placeholder='search robots'
-        onChange={searchChange}
-      />
+          onChange={searchChange}
+          id='search'
+        />
+        </label>
     </div>
   );
 }
